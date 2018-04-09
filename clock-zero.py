b972d1fd5102f46d6c696d33001790492b78dd12
Vlad Gheorghe <31849727+vlad-gheorghe@users.noreply.github.com>
2018-04-09 13:36:57 +0300

    Add files via upload

---
+++ clock-zero.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from tm1637 import TM1637
+from time import time, sleep, localtime
+
+DIO=14
+CLK=15
+
+def show_clock(tm):
+    t = localtime()
+    sleep(1 - time() % 1)
+
+    tm.numbers(t.tm_hour, t.tm_min, True)
+    sleep(.5)
+    tm.numbers(t.tm_hour, t.tm_min, False)
+
+print("\n")
+print("============================")
+print(" Starting clock application")
+print("============================")
+    
+tm = TM1637(CLK, DIO)
+tm.brightness(7)
+    
+while True:
+    show_clock(tm)
+
